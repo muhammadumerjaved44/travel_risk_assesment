25731e070a729782cbcdd60e488aa9ac1fec12fd
Muhammad Umer Javed <m.umer@g1g.com>
2020-03-30 17:08:00 +0500

    IT-22 - Write function to load the chrome from the local and docker environment

--- scrap_data_td_canada.py
+++ scrap_data_td_canada.py
@@ -23,3 +23,26 @@ basic_config = hp.from_env()
 selenium_url = basic_config.SELENIUM_URI
 geo_engine_conn = hp.get_geodb_engine()
 itira_engine_conn = hp.get_itiradb_engine()
+
+
+with itira_engine_conn.begin():
+    q = text('''
+         SELECT * FROM `countries`
+         ''')
+    result = itira_engine_conn.execute(q)
+    countries = pd.DataFrame(result.fetchall(), columns = result.keys())
+new_countries = countries[['country_id', 'country', 'abbr']]
+new_countries = hp.standerdise_country_name(new_countries, 'country')
+
+# load browser for scrapping
+try:
+    browser = hp.load_browser()
+    logging.info('run from the local browser')
+except:
+    browser = webdriver.Remote(
+              command_executor=selenium_url,
+              desired_capabilities=DesiredCapabilities.CHROME)
+    logging.info('run from the docker browser')
+
+data_list =  hp.prepare_urls_travel_advisory_canada()
+df = pd.DataFrame(data_list, columns=['new_country'])
